9d80992fe9e136ed8374677a9e2136f671b2b151
S1lence-z <j3.zelenka@gmail.com>
2026-04-09 22:15:25 +0200

    feat: add hotjar script tag dynamically

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,18 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
-
+	import { onMount } from 'svelte';
+	
 	let { children } = $props();
+
+	onMount(() => {
+		if (import.meta.env.VITE_HOTJAR_ENABLED === 'true') {
+			const script = document.createElement('script');
+			script.src = 'https://t.contentsquare.net/uxa/1ad894ba06ff0.js';
+			script.async = true;
+			document.head.appendChild(script);
+		}
+	});
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
